9cce155cbf29444e0e39c05246a71f416694040d
ushapmgithub <62152712+ushapmgithub@users.noreply.github.com>
2020-06-19 18:23:37 +0100

    First commit

---
+++ src/main/java/DemoRepo/DemoRepo/App.java
@@ -0,0 +1,13 @@
+package DemoRepo.DemoRepo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
